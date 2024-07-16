2421fb5d7290e0fbd65f3dda73bb063837a747bc
Rubenvill0811 <rubenvill0811@gmail.com>
2024-07-15 19:13:22 -0700

    conditional rendering

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,49 +1,47 @@
 import { Nav } from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Logo from '../../public/blueLogo.png';
-import Homepage from "../pages/homepage";
 
-
-
+import Logo from "../../public/blueLogo.png";
 
 export default function MainNav() {
   return (
     <div className="nav-container mb-5">
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
-            <Navbar.Brand href="/#home">
-                <img
-                    src={Logo}
-                    width="50"
-                    height="50"
-                    className="d-inline-block align-top m-auto"
-                    alt="Brand Name logo"
-                />{' '}
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbar-collapse-id" />
-            <Navbar.Collapse id="navbar-collapse-id">
-                <Nav>
-                    <Nav.Link as={Link} to='Invite'>
-                        Send invite
-                    </Nav.Link>
-                    <Nav.Link as={Link} to='Huddle'>
-                        Huddle
-                    </Nav.Link>
-                    <Nav.Link as={Link} to='Contact-us'>
-                        Contact Us
-                    </Nav.Link>
-                    <Nav.Link as={Link} to='Create-account'>
-                        Create account
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        <div className="content">
-
-        </div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        fixed="top"
+      >
+        <Navbar.Brand href="/#home">
+          <img
+            src={Logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top m-auto"
+            alt="Brand Name logo"
+          />{" "}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-collapse-id" />
+        <Navbar.Collapse id="navbar-collapse-id">
+          <Nav>
+            <Nav.Link as={Link} to="Invite">
+              Send invite
+            </Nav.Link>
+            <Nav.Link as={Link} to="Huddle">
+              Huddle
+            </Nav.Link>
+            <Nav.Link as={Link} to="Contact-us">
+              Contact Us
+            </Nav.Link>
+            <Nav.Link as={Link} to="Create-account">
+              Create account
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="content"></div>
     </div>
-  )
+  );
 }
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,51 +1,51 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx'
-import Overview from './pages/homepage.jsx'
-import Login from './pages/LoginPage.jsx'
-import PageNotFound from './pages/404.jsx';
-import Contact from './pages/Contact-us.jsx';
-import Huddle from './pages/Huddle.jsx';
-import Invite from './pages/Send-invite.jsx';
-import CreateAccount from './pages/Create-account.jsx';
-import LandingPage from './pages/LandingPage.jsx';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Overview from "./pages/homepage.jsx";
+import Login from "./pages/LoginPage.jsx";
+import PageNotFound from "./pages/404.jsx";
+import Contact from "./pages/Contact-us.jsx";
+import Huddle from "./pages/Huddle.jsx";
+import Invite from "./pages/Send-invite.jsx";
+import CreateAccount from "./pages/Create-account.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <PageNotFound />,
-      children: [
-        {
-          index: true,
-          element: <LandingPage />,
-        },
-        {
-          path: '/Invite',
-          element: <Invite />
-        },
-        {
-          path: '/Contact-us',
-          element: <Contact />
-        },
-        {
-          path: '/Huddle',
-          element: <Huddle />
-        },
-        {
-          path: '/Login',
-          element: <Login />
-        },
-        {
-          path: '/Create-account',
-          element: <CreateAccount />
-        },
-        {
-          path: '/overview',
-          element: <Overview />
-        },
-      ]
-    }
-  ])
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "/Invite",
+        element: <Invite />,
+      },
+      {
+        path: "/Contact-us",
+        element: <Contact />,
+      },
+      {
+        path: "/Huddle",
+        element: <Huddle />,
+      },
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+      {
+        path: "/Create-account",
+        element: <CreateAccount />,
+      },
+      {
+        path: "/overview",
+        element: <Overview />,
+      },
+    ],
+  },
+]);
 
-  export default router;
+export default router;
